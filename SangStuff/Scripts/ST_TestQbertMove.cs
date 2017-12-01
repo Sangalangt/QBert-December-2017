2017833458b04d5bd8322c28ecd66bce43ad0eba
Sangalangt <sangalangt@gmail.com>
2017-12-01 02:06:47 -0500

    Updated Personal Folder
    Personal Folder contains self edits to mainmenu, splashscreen, loading screen, pausemenu scene, imported and working qbert with movement and turns, also has updated scripts that I am currently responsible for. In order to avoid duplicates script folders should be cleaned up. Start of clean up. Moved animators that I have made into this folder as well.

---
+++ SangStuff/Scripts/ST_TestQbertMove.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ST_TestQbertMove : MonoBehaviour {
+   public GameObject QB;
+   public Vector3 spawnspot = new Vector3(0, 7.5f, 6);
+   public Vector3 curPosition;
+   // public float delay = 1;
+    private float timer;
+    //public MeshRenderer QBertRenderer;
+    public GameObject turncontrol;
+    // Use this for initialization
+    void Start () {
+        QB.transform.position = spawnspot;
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        curPosition = QB.transform.position;
+        
+
+        /*if (timer > delay)
+        {
+            QBertRenderer.enabled = true;
+        }*/
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            QB.transform.Translate(0, 0, -1);
+            turncontrol.transform.eulerAngles = new Vector3(0, 180, 0);
+            turnoffrenderer();
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            QB.transform.Translate(0, 1.3f, 1);
+            turncontrol.transform.eulerAngles = new Vector3(0, 0, 0);
+            turnoffrenderer();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            QB.transform.Translate(1, 0, 0);
+            turncontrol.transform.eulerAngles = new Vector3(0, 90, 0);
+            turnoffrenderer();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            QB.transform.Translate(-1, 1.3f, 0);
+            turncontrol.transform.eulerAngles = new Vector3(0, 270, 0);
+            turnoffrenderer();
+        }
+        
+    }
+    public void turnoffrenderer()
+    {
+        timer = 0;
+    //    QBertRenderer.enabled = false;
+    }
+
+}
---
+++ SangStuff/Scripts/TurnOnandOff.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TurnOnandOff : MonoBehaviour {
+    public GameObject instructiontext;
+    public GameObject close;
+    
+    // Use this for initialization
+    void Start()
+    {
+        instructiontext.SetActive(false);
+        close.SetActive(false);
+    }
+
+    public void Instructionsplease()
+    {
+        instructiontext.SetActive(true);
+        close.SetActive(true);
+    }
+
+    public void nomoreinstructions()
+    {
+        instructiontext.SetActive(false);
+        close.SetActive(false);
+    }
+}
