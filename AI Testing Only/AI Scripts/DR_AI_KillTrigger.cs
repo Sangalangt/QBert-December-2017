d294c15151ef3dcaa2088462559184b16a00611f
DevinRobaina <34114286+DevinRobaina@users.noreply.github.com>
2017-12-04 18:13:04 -0500

    Just renamed the folder because i dont care anymor
    just end it all (this is the real AI folder)

---
+++ AI Testing Only/AI Scripts/DR_AI_KillTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DR_AI_KillTrigger : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Kill Volume")
+        {
+            Object.Destroy(gameObject);
+            print("Object killed");
+        }
+    }
+}
